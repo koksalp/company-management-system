a853cae2e4504db8316b4dc3caf72aca319a5a43
koksalp <alp_koksal@hotmail.com>
2026-06-16 23:25:07 +0300

    protect routes when logged in

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,10 +6,10 @@ import Register from "./pages/register/Register";
 import Dashboard from "./pages/dashboard/Dashboard";
 
 import Company from "./pages/company/Company";
-import Product from "./pages/product/Product"; 
+import Product from "./pages/product/Product";
 import Navbar from "./components/navbar/Navbar";
 import ProtectedRoute from "./routes/ProtectedRoute";
-
+import PublicOnlyRoute from "./routes/PublicOnlyRoute";
 function App() {
   const { user, loading } = useAuth();
 
@@ -22,11 +22,23 @@ function App() {
       {user && <Navbar />}
 
       <Routes>
-      
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
+        <Route
+          path="/login"
+          element={
+            <PublicOnlyRoute>
+              <Login />
+            </PublicOnlyRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <PublicOnlyRoute>
+              <Register />
+            </PublicOnlyRoute>
+          }
+        />
 
- 
         <Route
           path="/"
           element={
